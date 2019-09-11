030d887591ef6cd7d3efa9e29f76633d41a411d6
Username <Username@example.com>
2019-09-11 11:16:34 +0800

    this version is more power than the previous one!lol

--- doubanclaws.py
+++ doubanclaws.py
@@ -13,6 +13,19 @@ def crawl(url):
         print("序号: "+num)
         name = tag.find(attrs={'class': 'title'}).get_text()
         print("电影名: "+name)
+        com = tag.find(attrs={'class': 'rating_num'}).get_text()
+        com = com.replace("\n", "")
+        print("评分："+com)
+        info = tag.find(attrs={'class': 'inq'})
+        if(info):
+            content = info.get_text()
+            print("影评： "+content)
+        print('\n')
+
 if __name__ == '__main__':
-    url = 'https://movie.douban.com/top250?start=0&filter='
-    crawl(url)
+    i = 0 #计数器
+    while i < 10:
+        num = i*25
+        url = 'https://movie.douban.com/top250?start=' + str(num) + '&filter='
+        crawl(url)
+        i += 1
